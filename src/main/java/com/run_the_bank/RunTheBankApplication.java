e799eb88978a030a38241611eca9d3e892f1f6af
welton-2020 <weltdias@gmail.com>
2024-01-09 19:26:25 -0300

    "Comitando o RunTheBankApplication e RunTheBankApplicationTest"

---
+++ src/main/java/com/run_the_bank/RunTheBankApplication.java
@@ -0,0 +1,13 @@
+package com.run_the_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunTheBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunTheBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/run_the_bank/RunTheBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.run_the_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunTheBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
